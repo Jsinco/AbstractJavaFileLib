dd2aac9fb7fe570cefc4771d66925c1dc576a2a9
Jsinco <jonahmarksinclair@gmail.com>
2024-12-08 06:52:51 -0500

    still use this nonsense on occasion

--- src/main/java/dev/jsinco/abstractjavafilelib/schemas/SnakeYamlConfig.java
+++ src/main/java/dev/jsinco/abstractjavafilelib/schemas/SnakeYamlConfig.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
 import java.io.Writer;
